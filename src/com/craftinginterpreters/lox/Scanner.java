75cd6dfea2035b85a1be52a59015cb11e5130065
soethiha <usersoe123@gmail.com>
2025-07-17 18:00:36 +0630

    comments

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -1,7 +1,10 @@
 package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import static com.craftinginterpreters.lox.TokenType.*;
 import com.craftinginterpreters.lox.lox.*;
 public class Scanner {
@@ -10,6 +13,29 @@ public class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+
+    }
 
     Scanner(String source) {
         this.source = source;
@@ -57,7 +83,10 @@ public class Scanner {
             case '/':
                 if (match('/')) {
                     while (peek() != '\n' && !isAtEnd()) advance();
-                } else {
+                } else if (match('*')) {
+                    while (peek() != '*' && peekNext() != '/') advance();
+                    current += 2;
+                }else {
                     addToken(SLASH);
                 }
                 break;
@@ -87,6 +116,19 @@ public class Scanner {
         return ( c >= 'a' && c <= 'z') ||
                ( c >= 'A' && c <= 'Z') ||
                ( c == '_');
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(IDENTIFIER);
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
     }
 
     boolean isDigit(char c) {
